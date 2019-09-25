203664ae40618462053c70f16a2b8e37dc83f574
yothiyoges <yothiyoges1996@gmail.com>
2019-09-25 14:56:04 +0530

    edited

---
+++ lib/models/entities/answer.dart
@@ -0,0 +1,12 @@
+import 'package:meta/meta.dart';
+
+class Answer {
+  final String question;
+
+  final String answer;
+
+  Answer({
+    @required this.question,
+    @required this.answer,
+  });
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2018 Harsh Sharma
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:project_3s_mobile/pages/login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), _handleTapEvent);
+    return Scaffold(
+      body: _splashContainer(),
+    );
+  }
+
+  Future<void> _handleTapEvent() async {
+    if (this.mounted) {
+      setState(() {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LogInPage()),
+        );
+      });
+    }
+  }
+
+//------------------------------------------------------------------------------
+  Widget _splashContainer() {
+    return GestureDetector(
+        onTap: _handleTapEvent,
+        child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(color: Colors.blue[400]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                    child: Image(
+                  height: 200.0,
+                  width: 200.0,
+                  image: AssetImage("assets/images/ic_launcher.png"),
+                  fit: BoxFit.fill,
+                )),
+                Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    "Hotel Reviewer",
+                    style: TextStyle(color: Colors.white, fontSize: 24.0),
+                  ),
+                ),
+              ],
+            )));
+  }
+}
