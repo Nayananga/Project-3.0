fb1837e9cbf52be541174747e7220ba137ef935e
yothiyoges <yothiyoges1996@gmail.com>
2019-09-26 21:06:45 +0530

    resolved

--- lib/models/ApiResponse.dart
+++ lib/models/ApiResponse.dart
@@ -1,33 +1,35 @@
-/*
- * Copyright 2018 Harsh Sharma
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+import 'dart:convert';
 
-//import 'package:project_3s_mobile/models/User.dart';
-//import 'package:json_annotation/json_annotation.dart';
-//
-//part 'ApiResponse.g.dart';
-//
-//@JsonSerializable()
-//class ApiResponse extends Object with _$ApiResponseSerializerMixin {
-//  String result;
-//  String message;
-//  User user;
-//
-//  ApiResponse({this.result, this.message, this.user});
-//
-//  factory ApiResponse.fromJson(Map<String, dynamic> json) =>
-//      _$ApiResponseFromJson(json);
-//}
+import 'package:http/http.dart' as http;
+import 'package:project_3s_mobile/models/User.dart';
+import 'package:project_3s_mobile/utils/app_shared_preferences.dart';
+
+class ApiResponce {
+  handleLoginResponse(http.Response response) {
+    if (response.statusCode == 200) {
+      final _responseData = jsonDecode(response.body);
+      print(_responseData['message']['Logged_User_Id']);
+      final String _googleId = _responseData['message']['Logged_User_Id'];
+      final String _nickname = _responseData['message']['Logged_User_Name'];
+      final User _user = User(
+          googleId: _googleId,
+          email: '',
+          nickname: _nickname,
+          image: '',
+          phoneNo: '',
+          nic: '');
+      AppSharedPreferences.setUserProfile(_user);
+    } else {
+      throw new Exception(response.statusCode);
+    }
+  }
+
+  handleCreateReviewResponse(http.Response response) {
+    if (response.statusCode == 200) {
+      final _responseData = jsonDecode(response.body);
+      print(_responseData['message']['Create_Review_Status']);
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
+}
