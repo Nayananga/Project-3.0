408a76b6381402459d0bf4a132ccd571e7d3b308
yothiyoges <yothiyoges1996@gmail.com>
2019-09-27 12:56:14 +0530

    complain page is created

--- lib/pages/complaint_page.dart
+++ lib/pages/complaint_page.dart
@@ -1,216 +1,186 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-// void main() => runApp(new MyApp());
+class ComplaintPage extends StatefulWidget {
+  @override
+  State createState() => Complaint();
+}
 
-// class MyApp extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//     return new MaterialApp(
-//       title : 'Flutter Demo',
-//       theme: new ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: new MyHomePage(
-//         ),
-//     );
-//   }
-// }
+class Complaint extends State<ComplaintPage> with TickerProviderStateMixin {
+ 
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:  new Color(0xffde5cbc),
+        title: Text("Complaint"),
+        elevation: Theme.of(ctx).platform == TargetPlatform.iOS ? 0.0 : 6.0,
+      ),
 
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _CircleProgressState createState() => new _CircleProgressState();
-// }
+      body: Container(
+           decoration: BoxDecoration(
+              color:new Color (0xff622F74),
+               gradient:LinearGradient(
+                 colors: [new Color (0xff6094e8),new Color(0xffde5cbc)],
+                     begin:Alignment.centerRight,
+                       end:Alignment.centerLeft, 
+               )),
 
-// // class _CircleProgressState extends State<MyHomePage> with SingleTickerProviderStateMixin{
-// //     AnimationController progressController ;
-// //     Animation<double> animation;
-
-// //     void initState(){
-// //       super.initState();
-// //       progressController = AnimationController(vsync:this, duration: Duration(milliseconds: 1000));
-// //       animation = Tween<double>(begin: 0,end :80).animate(progressController)..addListener((){
-// //         setState((){
-
-// //         });
-// //       });
-// //     }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.blueAccent,
-//         //centerTitle: true,
-//         leading: Icon(Icons.playlist_add_check),
-//         title: new Text(
-//           'Complaint',
-//           textAlign: TextAlign.center,
-//           style: TextStyle(
-//            fontFamily: "Herriweather-Black",
-//             fontWeight: FontWeight.bold,
-//              ),
-//         ),
-//         ),
-        
-//       body: Container(
-//           child: SafeArea(
-//             top:false,
-//             bottom: false,
-//             child: Container(
-//                 child:SingleChildScrollView(
-//                   padding:EdgeInsets.all(15.0),
+               child: SafeArea(
+                top:false,
+                bottom: false,
+                child: Container(
+                child:SingleChildScrollView(
+                  padding:EdgeInsets.all(15.0),
                   
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.stretch,
-//                      children: <Widget>[
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                     children: <Widget>[
                     
-//                      SizedBox(height: 5.0,),
+                     SizedBox(height: 5.0,),
 
-//                       Center(
-//                         child: Container(
-//                        // margin: EdgeInsets.all(10.0),
-//                         height:200.0,
-//                         width:300.0,
-//                         decoration:BoxDecoration(
-//                           color: Colors.white,
-//                           border:Border(
-//                             top:BorderSide(color: Colors.indigo, width: 3.0),
-//                             right:BorderSide(color: Colors.indigo, width: 3.0),
-//                             left:BorderSide(color: Colors.indigo, width: 3.0),
-//                             bottom:BorderSide(color: Colors.indigo, width: 3.0),),
+                      Center(
+                        child: Container(
+                            height:200.0,
+                            width:300.0,
 
-//                             borderRadius: BorderRadius.all(Radius.circular(20.0),
-//                           ),
-//                            ),
+                          decoration:BoxDecoration(
+                          color: Colors.white,
+                          border:Border(
+                            top:BorderSide(color: Colors.indigo, width: 3.0),
+                            right:BorderSide(color: Colors.indigo, width: 3.0),
+                            left:BorderSide(color: Colors.indigo, width: 3.0),
+                            bottom:BorderSide(color: Colors.indigo, width: 3.0),),
 
-              
-//                            child: Icon(Icons.photo_camera,
-//                            size:150.0,
-//                            color:Colors.grey.shade500,
-//                            ),
-//                         ),
-//                         ),
-
-//                SizedBox(
-//                  height: 30.0,
-//                ),
+                            borderRadius: BorderRadius.all(Radius.circular(20.0),
+                          )),
+                           child: Icon(Icons.photo_camera,
+                           size:150.0,
+                           color:Colors.grey.shade500,
+                           ))),
+                 SizedBox( height: 30.0),
                
-//                     TextFormField(
-//                       keyboardType: TextInputType.emailAddress,
-//                       decoration: InputDecoration(
-//                         enabledBorder: OutlineInputBorder(
-//                           borderSide: BorderSide(
-//                             color:Colors.indigo,
-//                             width:1.5,
-//                             style: BorderStyle.solid,
-//                           ),
-//                           borderRadius: BorderRadius.all(Radius.circular(15.0)),
-//                         ),
-//                          focusedBorder: OutlineInputBorder(
-//                             borderSide: BorderSide(
-//                             color:Colors.indigo,
-//                             width:1.5,
-//                             style: BorderStyle.solid,
-//                           ),
-//                           borderRadius: BorderRadius.all( Radius.circular(15.0)),
-//                         ),
-//                          prefixIcon: Icon(
-//                           Icons.my_location,
-//                           color:Colors.black,
-//                         ),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color:Colors.indigo,
+                            width:1.5,
+                            style: BorderStyle.solid,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        ),
+                         focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                            color:Colors.indigo,
+                            width:1.5,
+                            style: BorderStyle.solid,
+                          ),
+                          borderRadius: BorderRadius.all( Radius.circular(15.0)),
+                        ),
+                         prefixIcon: Icon(
+                          Icons.my_location,
+                          color:Colors.black,
+                        ),
 
-//                         labelText: "Location",
-//                         helperText: "Your location",
+                        labelText: "Location",
+                        helperText: "Your location",
                         
-//                         labelStyle: TextStyle(
-//                         color: Colors.indigo,
-//                         fontFamily: "Merriweather-Black",
-//                         fontWeight: FontWeight.bold,
-//                          ),
+                        labelStyle: TextStyle(
+                        color: Colors.indigo,
+                        fontFamily: "Merriweather-Black",
+                        fontWeight: FontWeight.bold,
+                         ),
 
-//                         helperStyle: TextStyle(
-//                         color: Colors.indigo,
-//                         fontFamily: "Merriweather-Black",
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                       ),
-//                       maxLines:1,
-//                       ),
-              
+                        helperStyle: TextStyle(
+                        color: Colors.indigo,
+                        fontFamily: "Merriweather-Black",
+                        fontWeight: FontWeight.bold,
+                      )),
+                      maxLines:1,
+                      ),
+                 SizedBox(height: 30.0),
 
-//                 SizedBox(
-//                  height: 30.0,
-//                ),
+                  TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color:Colors.indigo,
+                            width:1.5,
+                            style: BorderStyle.solid,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        ),
+                         focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                            color:Colors.indigo,
+                            width:1.5,
+                            style: BorderStyle.solid,
+                          ),
+                          borderRadius: BorderRadius.all( Radius.circular(15.0)),
+                        ),
+                         prefixIcon: Icon(
+                          Icons.message,
+                          color:Colors.black,
+                        ),
 
-//                TextFormField(
-//                       keyboardType: TextInputType.emailAddress,
-//                       decoration: InputDecoration(
-//                         enabledBorder: OutlineInputBorder(
-//                           borderSide: BorderSide(
-//                             color:Colors.indigo,
-//                             width:1.5,
-//                             style: BorderStyle.solid,
-//                           ),
-//                           borderRadius: BorderRadius.all(Radius.circular(15.0)),
-//                         ),
-//                          focusedBorder: OutlineInputBorder(
-//                             borderSide: BorderSide(
-//                             color:Colors.indigo,
-//                             width:1.5,
-//                             style: BorderStyle.solid,
-//                           ),
-//                           borderRadius: BorderRadius.all( Radius.circular(15.0)),
-//                         ),
-//                          prefixIcon: Icon(
-//                           Icons.message,
-//                           color:Colors.black,
-//                         ),
-
-//                         labelText: "Description",
-//                         helperText: "Enter your complaint",
+                        labelText: "Description",
+                        helperText: "Enter your complaint",
                         
-//                         labelStyle: TextStyle(
-//                         color: Colors.indigo,
-//                         fontFamily: "Merriweather-Black",
-//                         fontWeight: FontWeight.bold,
-//                          ),
+                        labelStyle: TextStyle(
+                        color: Colors.indigo,
+                        fontFamily: "Merriweather-Black",
+                        fontWeight: FontWeight.bold,
+                         ),
 
-//                         helperStyle: TextStyle(
-//                         color: Colors.indigo,
-//                         fontFamily: "Merriweather-Black",
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                       ),
-//                       maxLines:1,
-//                       ),
+                        helperStyle: TextStyle(
+                        color: Colors.indigo,
+                        fontFamily: "Merriweather-Black",
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                      maxLines:1,
+                      ),
               
-//                        Padding(
-//                        padding: const EdgeInsets.all(30.0),
-//                        child: Row(
-//                          mainAxisAlignment: MainAxisAlignment.center,
-//                          children: <Widget>[
+                       Padding(
+                       padding: const EdgeInsets.all(20.0),
+                       child: Row(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children: <Widget>[
 
-//                            FlatButton(
-//                              color:Colors.blueAccent,
-//                              shape:StadiumBorder(),
-//                              onPressed: (){},
-//                              child: Text("Done",
-//                                 style: TextStyle(
-//                                fontSize: 20.0,
-//                                color: Colors.white,
-//                              ),),
-//                            ),
-//                          ],
 
-//                        ),
-//                      ),
-                 
-//                      ],
-//         ),
-//           ),
-//         ),
-//           )
-//       )
-//     );
-//   }
-// }
+                          ButtonTheme(
+                              minWidth: 150.0,
+                              height: 50.0,
+                              child: RaisedButton(
+                                color:Colors.purpleAccent,
+                                shape:StadiumBorder(),
+                                onPressed: _handleDone,
+                                child: Text("Done",
+                                  style: TextStyle(
+                                   fontSize: 25.0,
+                                    color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),   
+
+
+                   ]
+                 )
+               )
+                ],
+        ))),
+    )),
+  );
+  }
+
+  Future<void> _handleDone() async {
+
+  }
+}
+
+
